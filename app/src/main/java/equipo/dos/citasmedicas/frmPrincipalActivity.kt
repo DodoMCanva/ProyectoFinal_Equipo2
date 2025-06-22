e101c5dbb831072647028ec3631d4eb6656ab0d9
PaulinaRayos <163494315+PaulinaRayos@users.noreply.github.com>
2025-06-22 00:59:38 -0700

    correccion de layouts y botones de calendario

--- app/src/main/java/equipo/dos/citasmedicas/frmPrincipalActivity.kt
+++ app/src/main/java/equipo/dos/citasmedicas/frmPrincipalActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class frmPrincipalPacienteActivity : AppCompatActivity() {
+class frmPrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_frm_principal_paciente)
+        setContentView(R.layout.activity_frm_principal)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
