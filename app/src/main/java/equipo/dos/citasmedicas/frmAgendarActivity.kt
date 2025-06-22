918a966d211ee54b8f9d3a2ccf793dc37e8f1db1
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2025-06-21 23:46:24 -0700

    Detallel de mocl

--- app/src/main/java/equipo/dos/citasmedicas/frmAgendarActivity.kt
+++ app/src/main/java/equipo/dos/citasmedicas/frmAgendarActivity.kt
@@ -7,5 +7,8 @@ class frmAgendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frm_agendar)
+
+
+
     }
 }
--- app/src/main/java/equipo/dos/citasmedicas/frmLoginActivity.kt
+++ app/src/main/java/equipo/dos/citasmedicas/frmLoginActivity.kt
@@ -1,6 +1,9 @@
 package equipo.dos.citasmedicas
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,6 +13,25 @@ class frmLoginActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_frm_login)
 
+        val btnIniciar : Button = findViewById(R.id.btnIniciarSesion)
+        val btnRegistrarse : TextView = findViewById(R.id.tvRegistrarse)
+        val btnCntr : TextView = findViewById(R.id.tvOlvidasteContra)
 
+        var intent : Intent
+
+        btnIniciar.setOnClickListener {
+            //Aqui va la logica de inicio
+            //buscar
+            //intent = Intent(this, frmPrincipal::class.java)
+            //put extra de el tipo de sesion
+        }
+        btnRegistrarse.setOnClickListener {
+            intent = Intent(this, frmPerfilActivity::class.java)
+            startActivity(intent)
+        }
+        btnCntr.setOnClickListener {
+            intent = Intent(this, frmRestablecerContrasenaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
