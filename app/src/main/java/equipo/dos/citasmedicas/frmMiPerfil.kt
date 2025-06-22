cd1f31a3abe7d93318b73e092a3b4f4baedfaed4
ValeriaEL <168052890+ValeriaEL@users.noreply.github.com>
2025-06-21 20:21:31 -0700

    editar

---
+++ app/src/main/java/equipo/dos/citasmedicas/frmMiPerfil.kt
@@ -0,0 +1,15 @@
+package equipo.dos.citasmedicas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class frmMiPerfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frm_mi_perfil)
+
+    }
+}
