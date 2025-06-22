b1e71e1f3204cfeb693d04f0b3220050d4f3489b
superQ78 <167489927+superQ78@users.noreply.github.com>
2025-06-21 21:39:26 -0700

    frm Registrar Medico y registrar

---
+++ app/src/main/java/equipo/dos/citasmedicas/frmRegistroMedico.kt
@@ -0,0 +1,16 @@
+package equipo.dos.citasmedicas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class frmRegistroMedico : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_frm_registro_medico)
+
+    }
+}
