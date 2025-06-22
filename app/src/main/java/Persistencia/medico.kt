498c3161a9077ba375286853da3a797976791866
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2025-06-22 13:46:40 -0700

    objetos

--- app/src/main/java/Persistencia/medico.kt
+++ app/src/main/java/Persistencia/medico.kt
@@ -4,4 +4,12 @@ data class medico(var nombre : String,
                   var correo : String,
                   var telefono : String,
                   var fechaNacimiento : String,
-                  var genero : String)
+                  var genero : String,
+                  var horario : horario,
+                  var cedula : String,
+                  var especialidad : String,
+                  var estado : String,
+                  var ciudad : String,
+                  var calle : String,
+                  var numero : String,
+                  var cp : String)
