dc9dddb6d12ce3b34c60d1b68e6c56c5ec63efee
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2025-06-22 13:12:32 -0700

    primera parte persistencia

---
+++ app/src/main/java/Persistencia/paciente.kt
@@ -0,0 +1,7 @@
+package Persistencia
+
+data class paciente(var nombre : String,
+                    var correo : String,
+                    var telefono : String,
+                    var fechaNacimiento : String,
+                    var genero : String)
