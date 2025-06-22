b6d72440b1da20b7796e0a3ebf619a8729c28eab
superQ78 <167489927+superQ78@users.noreply.github.com>
2025-06-22 01:05:20 -0700

    pantallaDetalleMedico

---
+++ app/src/main/java/equipo/dos/citasmedicas/frmDetalleCitaMedicoPendienteActivity.kt
@@ -0,0 +1,16 @@
+package equipo.dos.citasmedicas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class frmDetalleCitaMedicoPendienteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_frm_detalle_cita_medico_pendiente)
+
+    }
+}
