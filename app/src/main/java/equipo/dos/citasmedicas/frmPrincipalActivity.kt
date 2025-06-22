1ed0e40a61619bf2e7897ab9b1d693a6f0826b3b
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2025-06-22 16:41:21 -0700

    accion menu

--- app/src/main/java/equipo/dos/citasmedicas/frmPrincipalActivity.kt
+++ app/src/main/java/equipo/dos/citasmedicas/frmPrincipalActivity.kt
@@ -1,5 +1,7 @@
 package equipo.dos.citasmedicas
 
+import Persistencia.medico
+import Persistencia.paciente
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
@@ -12,26 +14,48 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 import equipo.dos.citasmedicas.databinding.ActivityFrmPrincipalBinding
 
 class frmPrincipalActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityFrmPrincipalBinding.inflate(layoutInflater)
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_frm_principal)
 
-
+        var sesion = intent.getSerializableExtra("sesion")
+        val tipoSesion : String = when (sesion){
+            is paciente -> "paciente"
+            is medico -> "medico"
+            else -> "no asignado"
+        }
 
         var drawerLayout = findViewById<DrawerLayout>(R.id.drawer_miscitas)
         var toolbar = findViewById<Button>(R.id.btnMenu)
+        var nav = findViewById<NavigationView>(R.id.navegacion_menu)
+
         toolbar.setOnClickListener(View.OnClickListener {
             drawerLayout.openDrawer(GravityCompat.START)
-            //var miscitas = drawerLayout.findViewById<>()
         })
+        val hv = nav.getHeaderView(0)
+        val btnCerrar = hv.find(R.id.btnMenuCerrarSesion)
+        val btnOpcion = hv.findViewById<Button>(R.id.btnMenuOpcion)
+        val btnMisCitas = hv.findViewById<Button>(R.id.btnMenuMisCitas)
+
+        if (tipoSesion == "paciente"){
+            btnOpcion.setText("")
+            //btnOpcion.setIcon
+            btnOpcion.setOnClickListener(){
+
+            }
+        }else{
+            btnOpcion.setOnClickListener(){
+
+            }
+        }
 
 
         val tipoUsuario = intent.getStringExtra("tipoUsuario") ?: "paciente"
