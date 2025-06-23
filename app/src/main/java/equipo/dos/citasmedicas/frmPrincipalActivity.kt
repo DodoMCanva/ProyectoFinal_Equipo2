11c9172787a6d367df18ec6757a9cc0f54ee72a7
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2025-06-22 17:03:43 -0700

    navegacion

--- app/src/main/java/equipo/dos/citasmedicas/frmPrincipalActivity.kt
+++ app/src/main/java/equipo/dos/citasmedicas/frmPrincipalActivity.kt
@@ -1,10 +1,17 @@
 package equipo.dos.citasmedicas
 
+<<<<<<< Updated upstream
+=======
+import Persistencia.medico
+import Persistencia.paciente
+import android.content.Intent
+>>>>>>> Stashed changes
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -28,10 +35,59 @@ class frmPrincipalActivity : AppCompatActivity() {
 
         var drawerLayout = findViewById<DrawerLayout>(R.id.drawer_miscitas)
         var toolbar = findViewById<Button>(R.id.btnMenu)
+<<<<<<< Updated upstream
+=======
+        var nav = findViewById<NavigationView>(R.id.navegacion_menu)
+>>>>>>> Stashed changes
         toolbar.setOnClickListener(View.OnClickListener {
             drawerLayout.openDrawer(GravityCompat.START)
             //var miscitas = drawerLayout.findViewById<>()
         })
+<<<<<<< Updated upstream
+=======
+
+        val menu = nav.menu
+        val opcion = menu.findItem(R.id.btnMenuOpcion)
+
+        if (tipoSesion == "paciente"){
+            opcion.setIcon(R.drawable.date48)
+            opcion.setTitle("Agendar")
+        }else{
+            opcion.setIcon(R.drawable.settings30)
+            opcion.setTitle("Ajustes de Consulta")
+        }
+        nav.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.btnMenuMisCitas -> {
+                    Toast.makeText(this, "Mis citas", Toast.LENGTH_SHORT).show()
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.btnMenuOpcion -> {
+                    if (tipoSesion == "paciente"){
+                        intent = Intent(this, frmAgendarActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        intent = Intent(this, AjustesConsultaActivity::class.java)
+                        startActivity(intent)
+                    }
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+
+                R.id.btnMenuCerrarSesion -> {
+                    Toast.makeText(this, "Sesión cerrada", Toast.LENGTH_SHORT).show()
+                    // Redirigir a login, limpiar preferencias, etc.
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+
+                else -> false
+            }
+        }
+>>>>>>> Stashed changes
+
+
 
 
         val tipoUsuario = intent.getStringExtra("tipoUsuario") ?: "paciente"
