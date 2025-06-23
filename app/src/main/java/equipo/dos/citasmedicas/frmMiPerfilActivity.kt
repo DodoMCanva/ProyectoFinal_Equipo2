298f964b362c52eb96629d93867b027fbb35dd96
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2025-06-22 21:45:01 -0700

    boton de perfil

--- app/src/main/java/equipo/dos/citasmedicas/frmMiPerfilActivity.kt
+++ app/src/main/java/equipo/dos/citasmedicas/frmMiPerfilActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -22,13 +23,22 @@ class frmMiPerfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frm_mi_perfil)
-
+        var p : paciente? = null
+        var m : medico? = null
         val sesion = intent.getSerializableExtra("sesion")
         val tipoSesion: String = when (sesion) {
             is paciente -> "paciente"
             is medico -> "medico"
             else -> "no asignado"
         }
+
+        val nombre : TextView = findViewById(R.id.perfilNombre)
+        val numero : TextView = findViewById(R.id.perfilNumero)
+        val fecha : TextView = findViewById(R.id.perfilFechaNa)
+        val genero : TextView = findViewById(R.id.perfilGenero)
+        val cerrar : TextView = findViewById(R.id.btnCerrarSesion)
+        val editar : TextView = findViewById(R.id.btnEditarPerfil)
+
 
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_mi_perfil)
         val toolbar = findViewById<Button>(R.id.btnMenu)
@@ -42,12 +52,37 @@ class frmMiPerfilActivity : AppCompatActivity() {
         val opcion = menu.findItem(R.id.btnMenuOpcion)
 
         if (tipoSesion == "paciente") {
+            p = sesion as paciente?
+            if (p != null) {
+                nombre.text = p.nombre
+                numero.text = p.correo
+                fecha.text = p.fechaNacimiento
+                genero.text = p.genero
+            }
             opcion.setIcon(R.drawable.date48)
             opcion.title = "Agendar"
         } else {
+            m = sesion as medico?
+            if (m != null) {
+                nombre.text = m.nombre
+                numero.text = m.correo
+                fecha.text = m.fechaNacimiento
+                genero.text = m.genero
+            }
             opcion.setIcon(R.drawable.settings30)
             opcion.title = "Ajustes de Consulta"
         }
+        editar.setOnClickListener(){
+            var inte : Intent = Intent(this, frmEditarActivity::class.java)
+            startActivity(inte)
+        }
+        cerrar.setOnClickListener(){
+            val inte = Intent(this, frmLoginActivity::class.java)
+            inte.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(inte)
+            finish()
+        }
+
 
         nav.setNavigationItemSelectedListener { item ->
             when (item.itemId) {
