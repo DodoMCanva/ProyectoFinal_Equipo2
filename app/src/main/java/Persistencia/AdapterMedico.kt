2439ac92dfa3f8af5d13ae2bb0581f43ea686927
PaulinaRayos <163494315+PaulinaRayos@users.noreply.github.com>
2025-06-22 21:42:34 -0700

    agendarcitamedico, falta la confrimacion de la cita

--- app/src/main/java/Persistencia/AdapterMedico.kt
+++ app/src/main/java/Persistencia/AdapterMedico.kt
@@ -13,24 +13,29 @@ import android.widget.TextView
 import androidx.activity.result.IntentSenderRequest
 import equipo.dos.citasmedicas.R
 import equipo.dos.citasmedicas.frmAgendarMedicoActivity
+import java.io.Serializable
 
-class AdapterMedico(context: Context, val lista: ArrayList<medico>): ArrayAdapter<medico>(context,0, lista) {
+class AdapterMedico(
+    context: Context,
+    val lista: ArrayList<medico>,
+) : ArrayAdapter<medico>(context, 0, lista) {
 
-    override fun getView(position: Int, converterView: View?, parent: ViewGroup ): View{
-        val vista= converterView?: LayoutInflater.from(context).inflate(R.layout.campo_medico, parent, false)
-        val me= lista[position]
-        vista.findViewById<TextView>(R.id.medicoEspecialidad).text= me.especialidad
-        vista.findViewById<TextView>(R.id.medicoNombre).text= me.nombre
-        val layoutPanel= vista.findViewById<LinearLayout>(R.id.btnCampoMedico)
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val vista = convertView ?: LayoutInflater.from(context).inflate(R.layout.campo_medico, parent, false)
 
+        val me = lista[position]
+        vista.findViewById<TextView>(R.id.medicoEspecialidad).text = me.especialidad
+        vista.findViewById<TextView>(R.id.medicoNombre).text = me.nombre
+
+        val layoutPanel = vista.findViewById<LinearLayout>(R.id.btnCampoMedico)
         var selecMedico: LinearLayout= vista.findViewById(R.id.btnCampoMedico)
         selecMedico.setOnClickListener {
-            var intent: Intent= Intent(context, frmAgendarMedicoActivity::class.java)
-            context!!.startActivity(intent)
+            val intent = Intent(context, frmAgendarMedicoActivity::class.java)
+            intent.putExtra("medico", me)
 
+            context!!.startActivity(intent)
         }
 
-       return vista
+        return vista
     }
-
 }
