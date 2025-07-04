d9045c35cfa2a2e3f232d24b3713fe1049f491c4
superQ78 <167489927+superQ78@users.noreply.github.com>
2025-07-03 21:33:52 -0700

    subir foto desde cloudinary jijiji

--- app/src/main/java/equipo/dos/citasmedicas/Fragmentos/MiPerfilFragment.kt
+++ app/src/main/java/equipo/dos/citasmedicas/Fragmentos/MiPerfilFragment.kt
@@ -5,12 +5,14 @@ import Persistencia.paciente
 import Persistencia.sesion
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import android.widget.Toast
 import equipo.dos.citasmedicas.R
 import equipo.dos.citasmedicas.frmEditarActivity
@@ -31,47 +33,57 @@ class MiPerfilFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         imgFotoPerfil = view.findViewById(R.id.imgFotoPerfil)
 
-        var p : paciente? = null
-        var m : medico? = null
+        var p: paciente? = null
+        var m: medico? = null
 
-        val nombre : TextView = view.findViewById(R.id.perfilNombre)
-        val numero : TextView = view.findViewById(R.id.perfilNumero)
-        val fecha : TextView = view.findViewById(R.id.perfilFechaNa)
-        val genero : TextView = view.findViewById(R.id.perfilGenero)
-        val cerrar : TextView = view.findViewById(R.id.btnCerrarSesion)
-        val editar : TextView = view.findViewById(R.id.btnEditarPerfil)
+        val nombre: TextView = view.findViewById(R.id.perfilNombre)
+        val numero: TextView = view.findViewById(R.id.perfilNumero)
+        val fecha: TextView = view.findViewById(R.id.perfilFechaNa)
+        val genero: TextView = view.findViewById(R.id.perfilGenero)
+        val cerrar: TextView = view.findViewById(R.id.btnCerrarSesion)
+        val editar: TextView = view.findViewById(R.id.btnEditarPerfil)
 
+        Log.d("MiPerfilFragment", "Cargando perfil. Foto URL en sesion global: ${(Persistencia.sesion.obtenerSesion() as? medico)?.fotoPerfil ?: (Persistencia.sesion.obtenerSesion() as? paciente)?.fotoPerfil}")
         when (val s = sesion.obtenerSesion()) {
             is paciente -> {
                 nombre.text = s.nombre
                 numero.text = s.correo
                 fecha.text = s.fechaNacimiento
                 genero.text = s.genero
-                val fotoResId = resources.getIdentifier(s.fotoPerfil, "drawable", requireContext().packageName)
-                if (fotoResId != 0) {
-                    imgFotoPerfil.setImageResource(fotoResId)
-                }
+
+                Glide.with(this)
+                    .load(s.fotoPerfil)
+                    .placeholder(R.drawable.usuario)
+                    .error(R.drawable.usuario)
+                    .into(imgFotoPerfil)
+
             }
+
             is medico -> {
                 nombre.text = s.nombre
                 numero.text = s.correo
                 fecha.text = s.fechaNacimiento
                 genero.text = s.genero
-                val fotoResId = resources.getIdentifier(s.fotoPerfil, "drawable", requireContext().packageName)
-                if (fotoResId != 0) {
-                    imgFotoPerfil.setImageResource(fotoResId)
-                }
+                Glide.with(this)
+                    .load(s.fotoPerfil)
+                    .placeholder(R.drawable.usuario)
+                    .error(R.drawable.usuario)
+                    .into(imgFotoPerfil)
+                Log.d("MiPerfilFragment", "Cargando perfil. Foto URL en sesion global: ${(Persistencia.sesion.obtenerSesion() as? medico)?.fotoPerfil ?: (Persistencia.sesion.obtenerSesion() as? paciente)?.fotoPerfil}")
+
             }
+
             else -> {
-                Toast.makeText(requireContext(), "no se cargo correctamente la sesion", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "No se cargó correctamente la sesión", Toast.LENGTH_SHORT).show()
+                imgFotoPerfil.setImageResource(R.drawable.usuario)
             }
         }
 
-        editar.setOnClickListener(){
-            var inte : Intent = Intent(requireContext(), frmEditarActivity::class.java)
+        editar.setOnClickListener() {
+            var inte: Intent = Intent(requireContext(), frmEditarActivity::class.java)
             startActivity(inte)
         }
-        cerrar.setOnClickListener(){
+        cerrar.setOnClickListener() {
             val inte = Intent(requireContext(), frmLoginActivity::class.java)
             inte.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             sesion.cerrarSesion()
