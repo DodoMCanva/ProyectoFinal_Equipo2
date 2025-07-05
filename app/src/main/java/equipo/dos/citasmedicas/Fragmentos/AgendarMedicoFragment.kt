8c3e07a89cbbd58080497fc93926b29dcf2c4578
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2025-07-05 02:14:02 -0700

    ajuste agendar

--- app/src/main/java/equipo/dos/citasmedicas/Fragmentos/AgendarMedicoFragment.kt
+++ app/src/main/java/equipo/dos/citasmedicas/Fragmentos/AgendarMedicoFragment.kt
@@ -1,12 +1,7 @@
 package equipo.dos.citasmedicas.Fragmentos
 
-import Persistencia.cita
 import Persistencia.medico
-import Persistencia.paciente
-import Persistencia.sesion
 import android.app.DatePickerDialog
-import android.app.Dialog
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -15,21 +10,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.DatePicker
-import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import com.google.android.material.datepicker.MaterialDatePicker.Builder.datePicker
-import com.google.firebase.database.FirebaseDatabase
 import equipo.dos.citasmedicas.R
 import equipo.dos.citasmedicas.frmPrincipalActivity
 import modulos.ModuloHorario
 import java.util.Calendar
 
+@RequiresApi(Build.VERSION_CODES.O)
 class AgendarMedicoFragment : Fragment() {
     val modulo = ModuloHorario()
 
@@ -40,16 +32,24 @@ class AgendarMedicoFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_agendar_medico, container, false)
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
+
     private lateinit var spHora: Spinner
     private lateinit var tvFecha: TextView
     private lateinit var tvHoraSeleccionada: TextView
+    private lateinit var tvNombreMedico: TextView
+    private lateinit var tvCosto: TextView
+    var m : medico? = null
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        m = arguments?.getSerializable("medico") as? medico
         spHora = view.findViewById(R.id.spHora)
         tvFecha = view.findViewById(R.id.tvAgendarFecha)
         tvHoraSeleccionada = view.findViewById(R.id.tvHoraSeleccionada)
+        tvNombreMedico  = view.findViewById(R.id.tvAgendarNombre)
+        tvCosto = view.findViewById(R.id.tvMontoAgendar)
+
+        tvNombreMedico.setText(m?.nombre)
+        tvCosto.setText(m?.costoConsulta.toString())
 
         view.findViewById<ImageButton>(R.id.btnCalendario).setOnClickListener {
             mostrarSelectorFecha()
@@ -61,9 +61,10 @@ class AgendarMedicoFragment : Fragment() {
             }
             override fun onNothingSelected(parent: AdapterView<*>) {}
         }
+
+
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     private fun mostrarSelectorFecha() {
         val cal = Calendar.getInstance()
         val datePicker = DatePickerDialog(requireContext(),
@@ -83,27 +84,28 @@ class AgendarMedicoFragment : Fragment() {
 
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     private fun configurarSpinnerHoras(year: Int, month: Int, day: Int) {
         val cal = Calendar.getInstance().apply { set(year, month, day) }
         val dow = cal.get(Calendar.DAY_OF_WEEK)
-        val medico = arguments?.getSerializable("medico") as? medico ?: return
 
-        modulo.obtenerConfiguracionDelMedico(medico.uid!!) { config ->
-            if (config == null) {
-                Toast.makeText(context, "Error al cargar configuración", Toast.LENGTH_SHORT).show()
-                return@obtenerConfiguracionDelMedico
-            }
-            val (mañanaP, tardeP) = modulo.obtenerConfigDelDia(config, dow)
-            val horas = mutableListOf<String>()
-            if (mañanaP.first) horas += modulo.generarHorasEnHorario(mañanaP.second.desde, mañanaP.second.hasta)
-            if (tardeP.first) horas += modulo.generarHorasEnHorario(tardeP.second.desde, tardeP.second.hasta)
-            if (horas.isEmpty()) {
-                Toast.makeText(context, "No hay horarios disponibles", Toast.LENGTH_SHORT).show()
-                spHora.adapter = null
-            } else {
-                spHora.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, horas).apply {
-                    setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val uid = m?.uid
+        if (uid != null) {
+            modulo.obtenerConfiguracionDelMedico(uid) { config ->
+                if (config == null) {
+                    Toast.makeText(context, "Error al cargar configuración", Toast.LENGTH_SHORT).show()
+                    return@obtenerConfiguracionDelMedico
+                }
+                val (mañanaP, tardeP) = modulo.obtenerConfigDelDia(config, dow)
+                val horas = mutableListOf<String>()
+                if (mañanaP.first) horas += modulo.generarHorasEnHorario(mañanaP.second.desde, mañanaP.second.hasta)
+                if (tardeP.first) horas += modulo.generarHorasEnHorario(tardeP.second.desde, tardeP.second.hasta)
+                if (horas.isEmpty()) {
+                    Toast.makeText(context, "No hay horarios disponibles", Toast.LENGTH_SHORT).show()
+                    spHora.adapter = null
+                } else {
+                    spHora.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, horas).apply {
+                        setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    }
                 }
             }
         }
