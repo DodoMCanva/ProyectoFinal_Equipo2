dd546f166be9836e2bd432f2eac215e83048cdbb
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2025-07-09 22:11:37 -0700

    regresar

--- app/src/main/java/modulos/MenuDesplegable.kt
+++ app/src/main/java/modulos/MenuDesplegable.kt
@@ -223,12 +223,12 @@ object MenuDesplegable {
             is HistorialFragment -> "HistorialFragment"
             is AgendarFragment -> "AgendarFragment"
             //descomentarear si se quiere implementar
-            //is AgendarMedicoFragment -> "AgendarMedicoFragment"
+            is AgendarMedicoFragment -> "AgendarMedicoFragment"
             is AjusteConsultaFragment -> "AjusteConsultaFragment"
             is MiPerfilFragment -> "MiPerfilFragment"
             //descomentarear si se quiere implementar
-            //is DetalleCitaPacienteFragment -> "DetalleCitaPacienteFragment"
-            //is DetalleCitaMedicoFragment -> "DetalleCitaMedicoFragment"
+            is DetalleCitaPacienteFragment -> "DetalleCitaPacienteFragment"
+            is DetalleCitaMedicoFragment -> "DetalleCitaMedicoFragment"
             else -> "CitasFragment"
         }
     }
